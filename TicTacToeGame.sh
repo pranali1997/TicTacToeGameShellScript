18aee12fd3bc0b3485d64781fcae78a02807e538
Pranali0908 <pranali.lembhe77@gmail.com>
2019-12-16 14:45:35 +0530

    Added : displayBoard() and checkForValidity() functions to diplay board and check if user is giving valide input

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -6,11 +6,13 @@ echo "WELCOME TO TIC TAC TOE GAME"
 player=0
 computer=0
 
+declare -a board
+
 function resetBoard()
 {
 	for (( i=1; i<=9; i++ ))
 	do
-		board[(($i))]=$i
+		board[$i]=$i
 	done
 }
 resetBoard
@@ -27,4 +29,42 @@ function toss()
 		echo "computer will play first"
 	fi
 }
-toss
+#toss
+
+function displayBoard()
+{
+		echo " --- --- --- "
+		echo "| ${board[1]} | ${board[2]} | ${board[3]} |"
+		echo " --- --- --- "
+		echo "| ${board[4]} | ${board[5]} | ${board[6]} |"
+		echo " --- --- --- "
+		echo "| ${board[7]} | ${board[8]} | ${board[9]} |"
+		echo " --- --- --- "
+
+}
+
+function checkForValidation()
+{
+	displayBoard
+	flag=0
+	read -p "enter a number between 1 to 9 " position
+	for (( i=1;i<=9;i++ ))
+	do
+		if [ $position -eq $i ]
+		then
+			flag=1
+			break
+		else
+			flag=0
+		fi
+	done
+
+	if [ $flag -eq 1 ]
+	then
+	 	board[$i]=X
+		displayBoard  ${board[$i]}
+	else
+		checkForValidation
+	fi
+}
+checkForValidation
